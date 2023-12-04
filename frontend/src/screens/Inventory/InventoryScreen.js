4a802158ba3282828aa44f3a33ffc7df74fd7fa9
suvanbalu <suvanbalu@gmail.com>
2023-12-04 21:26:52 +0530

    added products view, need to delete and update

--- frontend/src/screens/Inventory/InventoryScreen.js
+++ frontend/src/screens/Inventory/InventoryScreen.js
@@ -1,8 +1,28 @@
 import React from 'react'
+import { IoMdAdd } from "react-icons/io";
+import { useNavigate } from 'react-router-dom';
+import InventoryTable from './inventoryTable';
+import dummyData from './dummydata.js';
+import PageTitle from '../../components/PageTitle';
+import CustomButton from '../../components/CustomButton';
 
 const InventoryScreen = () => {
+  const navigate = useNavigate();
+
   return (
-    <div>InventoryScreen</div>
+    <div className='pl-4 pr-12 flex flex-col gap-4 w-full'>
+      <div className='flex justify-between items-center'>
+        <PageTitle title={'All Purchases'} />
+        <CustomButton
+          onClick={() => { navigate('/purchases/add') }}
+          icon={<IoMdAdd />}
+          text={'Add New Purchase'}
+        />
+      </div>
+
+      <InventoryTable data={dummyData} />
+
+    </div>
   )
 }
 
