0f662655f607c8af7bb6e2f1ba5a073c46b4ad8a
suvanbalu <suvanbalu@gmail.com>
2023-12-17 13:24:05 +0530

    global headers set

--- backend/server.js
+++ backend/server.js
@@ -30,7 +30,23 @@ app.use(express.static(path.join(__dirname, '../frontend/build')));
 app.listen(PORT, () => {
   console.log("Server Listening to PORT - ", PORT);
 });
+const whitelist = [
+  '*'
+];
 
+app.use((req, res, next) => {
+  const origin = req.get('referer');
+  const isWhitelisted = whitelist.find((w) => origin && origin.includes(w));
+  if (isWhitelisted) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,Content-Type,Authorization');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+  }
+  // Pass to next layer of middleware
+  if (req.method === 'OPTIONS') res.sendStatus(200);
+  else next();
+});
 app.use("/api/sale", sales);
 app.use("/api/product", product);
 app.use("/api/purchase", purchase);
